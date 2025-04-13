aa84d3b81857458138401aac8bf6a46fb34ec637
mariawasnotfound <145267261+mariawasnotfound@users.noreply.github.com>
2025-04-13 19:22:26 +0500

    one select added

--- Select.sql
+++ Select.sql
@@ -1,1 +1,7 @@
+-- 1. Список сотрудников, работающих продавцами, отсортированный по дате принятия на работу
+SELECT emp.first_name, emp.last_name, emp.middle_name, pos.name AS position
+FROM Employees emp
+JOIN Positions pos ON emp.position = pos.position_id
+WHERE pos.name IN ('Продавец', 'Главный продавец')
+ORDER BY emp.hire_date;
 
